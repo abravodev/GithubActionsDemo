a5253e4fb3c9c900686f95995a3dbba10a9fddfb
A Bravo Dev <abravodev@users.noreply.github.com>
2021-05-08 20:41:54 +0200

    Add library with test project

---
+++ GitHubActionsDemo.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GitHubActionsDemo.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GitHubActionsDemo.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02aa8f50-ffcc-4309-b91b-0d3916d863bb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ GitHubActionsDemo.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+
+namespace GitHubActionsDemo.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        [TestCategory("UnitTests")]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(expected: 1, actual: Class1.Property1);
+        }
+
+        [TestMethod]
+        [TestCategory("IntegrationTests")]
+        public void TestMethod2()
+        {
+            Assert.IsNotNull(Path.GetTempPath());
+        }
+    }
+}
